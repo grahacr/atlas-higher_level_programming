d1d5631a7298cacc91abeae7345be5e2701b4757
Courtney Graham <7194@holbertonstudents.com>
2024-01-05 11:52:06 -0800

    isolate digits in randomized number and output based on value of last digit

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+string = str(number)
+last_digit = int(string[-1])
+if last_digit > 5:
+    print(f"Last digit of {string} is {last_digit} and is greater than 5")
+if last_digit == 0:
+    print(f"Last digit of {string} is {last_digit} and is 0")
+if 0 < last_digit < 6:
+    print(f"Last digit of {string} is {last_digit} and is less than 6 and not 0")
