68ed3e56ed113b41769c58df37d904659840a4c2
Courtney Graham <7194@holbertonstudents.com>
2024-01-05 13:24:59 -0800

    fix output with changing parantheses

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-last_digit = abs(number) % 10
+last_digit = abs(number % 10)
 print(f"Last digit of {number} is {last_digit}", end=" ")
 if last_digit > 5:
     print("and is greater than 5")
