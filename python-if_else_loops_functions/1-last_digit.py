4f5d717a59ed2c49833de40496e5d2109668ba83
Courtney Graham <7194@holbertonstudents.com>
2024-01-05 11:55:30 -0800

    fix output by fixing last if statement

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,5 +7,5 @@ if last_digit > 5:
     print(f"Last digit of {string} is {last_digit} and is greater than 5")
 if last_digit == 0:
     print(f"Last digit of {string} is {last_digit} and is 0")
-if 0 < last_digit < 6:
+if last_digit != 0 and last_digit < 6:
     print(f"Last digit of {string} is {last_digit} and is less than 6 and not 0")
