559b84d1b4cb3a03d56d678691541e42b7d43dab
Courtney Graham <7194@holbertonstudents.com>
2024-01-05 16:15:31 -0800

    simplify syntax for correct output

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i < 99:
-        print("{:02}".format(i), end=", ")
-    if i == 99:
-        end="\n"
+    print("{:02}".format(i), end=", " if i < 99 else "\n")
