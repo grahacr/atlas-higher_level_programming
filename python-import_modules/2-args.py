6789396fda79921f8007d9830e84aeab01d42fda
Courtney Graham <7194@holbertonstudents.com>
2024-01-07 21:24:43 -0800

    print number of and list of arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    arguments = len(argv) - 1
+    if arguments == 0:
+        print("0 arguments.")
+    elif arguments == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(arguments))
+    for i in range(1, arguments + 1):
+        print("{:d}: {:s}".format(i, arguments[i]))
