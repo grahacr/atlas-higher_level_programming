ea7ce71fe036b2533b0e3b2e41579d780df4b951
Courtney Graham <7194@holbertonstudents.com>
2024-01-07 16:24:47 -0800

    interrupted focus and lost track of names used, fixed typos

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for dig1 in range(10):
-    for dig2 in range(10 + 1, 10):
+    for dig2 in range(dig1 + 1, 10):
         if dig1 * 10 + dig2 < 89:
-            print("{:02d}".format(i * 10 + j), end=", ")
+            print("{:02d}".format(dig1 * 10 + dig2), end=", ")
         else:
-            print("{:02d}".format(i * 10 + j))
+            print("{:02d}".format(dig1 * 10 + dig2))
