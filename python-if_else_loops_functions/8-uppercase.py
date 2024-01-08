1cad3b520acfe19c9c3e13f061f45de4a042147f
Courtney Graham <7194@holbertonstudents.com>
2024-01-07 16:50:50 -0800

    chmod

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    upper_str = ""
+    for char in str:
+        if ord('a') <= ord(char) <= ord('z'):
+            upper_char = chr(ord(char) - ord('a') + ord('A'))
+        else:
+            upper_char = char
+        upper_str += upper_char
+        print(upper_str)
