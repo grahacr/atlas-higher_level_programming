fa49bb32d22a2c9cff0488436fd5d87f63892480
Courtney Graham <7194@holbertonstudents.com>
2024-01-07 21:41:53 -0800

    correct output hopefully with new import

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from sys import argv
+import sys
 if __name__ == "__main__":
     sum = 0
-    for i in range(1, len(argv)):
+    for i in range(1, len(sys.argv)):
         sum += int(sys.argv[i])
     print("{}".format(sum))
