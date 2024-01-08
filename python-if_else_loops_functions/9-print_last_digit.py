159ca807efdc550a4858281ae063627d69298d5c
Courtney Graham <7194@holbertonstudents.com>
2024-01-07 19:46:48 -0800

    fix output using modulo and absolute value instead oops

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    num_str = str(number)
-    last_digit = num_str[-1]
+    last_digit = abs(number) % 10
+    print("{}".format(last_digit), end='')
     return last_digit
