4414c69a8dd0d32c64e7099d0ef04d8773ce4cba
Courtney Graham <7194@holbertonstudents.com>
2024-01-09 12:50:28 -0800

    fix unneccessary list comprehension for output

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -4,5 +4,6 @@ def new_in_list(my_list, idx, element):
         return my_list.copy()
 
 
-new_list = [element if i == idx else value for i]
+new_list = my_list.copy()
+new_list[idx] == element
 return new_list
