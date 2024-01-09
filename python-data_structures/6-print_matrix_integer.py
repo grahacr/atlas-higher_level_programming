ae097417998c4907bcea57fbd3b6f4d2366ab9dd
Courtney Graham <7194@holbertonstudents.com>
2024-01-09 13:39:37 -0800

    take out testing function from example

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,13 +4,3 @@ def print_matrix_integer(matrix=[[]]):
         for element in row:
             print("{:d}".format(element), end='\t')
         print()
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
