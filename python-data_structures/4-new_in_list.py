09752b2296f8e4f57eab10baa0e3d6d853f153be
Courtney Graham <7194@holbertonstudents.com>
2024-01-09 12:46:48 -0800

    fix conditions if idx is negative, etc and return statements

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if not my_list:
-        return
-    new_list = [element if i == idx else value for i]
+    if idx < 0 or idx >= len(my_list):
+        return my_list.copy()
+
+
+new_list = [element if i == idx else value for i]
+return new_list
