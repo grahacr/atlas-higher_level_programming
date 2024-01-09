cedfec1e05c5825e77aeebe7e1ac08db7f207dd7
Courtney Graham <7194@holbertonstudents.com>
2024-01-09 13:42:12 -0800

    add edge case for checker

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in row:
-            print("{:d}".format(element), end='\t')
+            print("{:d}".format(element), end='')
+            if index < len(row) - 1:
+                print(' ', end='')
         print()
