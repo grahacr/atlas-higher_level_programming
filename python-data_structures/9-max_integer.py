66f9e278244c77903d5d63ad70f8b81ca2b8872e
Courtney Graham <7194@holbertonstudents.com>
2024-01-09 14:27:51 -0800

    return largest integer in list

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,9 +2,9 @@
 def max_integer(my_list=[]):
     if not my_list:
         return
-    largest_int = my_list[0]
-    for num in my_list[1:0]:
-        if num > largest_int:
-            largest_int = num
-
-    return largest_int
+    else:
+        largest_int = my_list[0]
+        for num in my_list[1:]:
+            if num > largest_int:
+                largest_int = num
+        return largest_int
