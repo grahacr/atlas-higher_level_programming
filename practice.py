6a448b90cf6cb8ff90885705d395c7b863d3bdab
Courtney Graham <7194@holbertonstudents.com>
2024-01-08 19:08:34 -0800

    new repository for data structures project - first function should print all integers of a given list

---
+++ practice.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+import math
+numlist = []
+for i in range(10):
+    numlist.append(random.randrange(1, 10))
+
+for i in numlist:
+    print(i)
---
+++ python-data_structures/README.md
@@ -0,0 +1,1 @@
+This project is for learning python data structures, including lists and tuples
