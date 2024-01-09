05792c604b8f4f969b141943dbe6546c1c7585a4
Courtney Graham <7194@holbertonstudents.com>
2024-01-09 13:22:55 -0800

    fix conditional statement for output

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ''.join([char for char in my_string if char != 'c' or 'C'])
+    new_string = ''.join([char for char in my_string if char != 'c' and char != 'C'])
     return new_string
