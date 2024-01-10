8d4948e0acaec21267ccd7603c5fdaaad583df2a
Courtney Graham <7194@holbertonstudents.com>
2024-01-09 18:28:38 -0800

    utilize map function to try and please checker

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+def square(i):
+    product = i * i
+    return product
+
+
 def square_matrix_simple(matrix=[]):
     new_matrix = matrix[:][:]
 
     for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            new_matrix[i][j] = new_matrix[i][j] ** 2
+        new_matrix.append(map(square, matrix[i]))
     return new_matrix
