8b2889b416abab7b18b6dc9c8abb8192690fac7d
Courtney Graham <7194@holbertonstudents.com>
2024-01-09 20:07:40 -0800

    fix edge case/ensure key is in dictionary. no use for extra variable

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    delete_this = key
-    del a_dictionary[delete_this]
+    if key in a_dictionary:
+        del a_dictionary[key]
     return a_dictionary
