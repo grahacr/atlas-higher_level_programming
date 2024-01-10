612dfe5a6d0bcf6751775dc8f62a39b4dfcff9b9
Courtney Graham <7194@holbertonstudents.com>
2024-01-09 20:41:34 -0800

    utilize .item feature and fix syntax for output

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    else:
+        best_score_key, best_score_value = next(iter(a_dictionary.items()))
+        for key, value in a_dictionary.items():
+            if value > best_score_value:
+                best_score_key, best_score_value = key, value
+        return best_score_key
