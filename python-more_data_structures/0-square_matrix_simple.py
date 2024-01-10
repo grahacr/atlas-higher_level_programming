d05c6f10aeab2d20248c2124d1654f8b7ce2c9cf
Courtney Graham <7194@holbertonstudents.com>
2024-01-09 18:22:26 -0800

    replace copy function to properly make copy of matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = copy.matrix()
+    new_matrix = matrix[:][:]
+
     for i in range(len(new_matrix)):
         for j in range(len(new_matrix[i])):
             new_matrix[i][j] = new_matrix[i][j] ** 2
     return new_matrix
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
