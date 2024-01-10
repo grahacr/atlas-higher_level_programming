b2f3281786b324ca6fb959310c1897fbd19de53a
Courtney Graham <7194@holbertonstudents.com>
2024-01-09 18:26:05 -0800

    fix typo

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,7 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = matrix[:][:]
 
-    for i in range(len(new_matrix)):
-        for j in range(len(new_matrix[i])):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
             new_matrix[i][j] = new_matrix[i][j] ** 2
     return new_matrix
