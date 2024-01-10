486c8626eff10c4f2d4ecbe5b2ebba695738fea0
Courtney Graham <7194@holbertonstudents.com>
2024-01-09 18:23:40 -0800

    trying to please checker

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -6,12 +6,3 @@ def square_matrix_simple(matrix=[]):
         for j in range(len(new_matrix[i])):
             new_matrix[i][j] = new_matrix[i][j] ** 2
     return new_matrix
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
