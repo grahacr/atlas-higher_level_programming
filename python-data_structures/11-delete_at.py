9d4b60523431a8e8d4144d6d02b515eb67fcf8d7
Courtney Graham <7194@holbertonstudents.com>
2024-01-09 17:06:24 -0800

    try different deleting method

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -7,5 +7,5 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
     else:
-        new_list = [elem for elem in my_list if elem != idx]
-        return new_list
+        del my_list[idx]
+        return my_list
