e44a025295dfd861efa0359bed28a02e4ced0efd
grahacr <courtneygraham918@gmail.com>
2024-01-15 01:09:07 +0000

    fix spacing for documenting module

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""This module defines a class named Square with variables and functions"""
+""" This module defines a class named Square with variables and functions
+"""
 
 
 class Square:
-    """This class defines a square with size"""
+    """ This class defines a square with size"""
     def __init__(self, size=0):
-        """This method initiates a square with integer size above 0"""
+        """ This method initiates a square with integer size above 0"""
         if size.isfloat():
             self.__size = size
             elif size < 0:
@@ -13,7 +14,7 @@ class Square:
             elif:
                 raise TypeError("size must be an integer")
     def area(self):
-        """This method returns the square area of size"""
+        """ This method returns the square area of size"""
         if self.size:
             sqArea = size x size
             return sqArea
