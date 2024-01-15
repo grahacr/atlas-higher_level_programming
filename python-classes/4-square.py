b7d96d283aa91d988819c22be3cf51e7d687a806
grahacr <courtney.graham@atlasschool.com>
2024-01-15 20:42:32 +0000

    use getter and setter and property to write a class square that defines a square with certain criteria

---
+++ python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+"""
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+    
+    @property
+    def size(self):
+        return self.__size
+    
+    @property
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+    
+    def area(self):
+        return self.__size * self.__size
+
+my_square = Square(89)
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+my_square.size = 3
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+try:
+    my_square.size = "5 feet"
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+except Exception as e:
+    print(e)
