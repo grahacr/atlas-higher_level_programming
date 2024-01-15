4a20a9d2c7a81d39a7529be33d3d47a8ef2f268f
grahacr <courtney.graham@atlasschool.com>
2024-01-15 21:21:22 +0000

    print square using hash sign

---
+++ python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+"""
+class Square:
+    """ Class defines a square """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """ method to retrieve size of Square """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """ method sets size of Square to new value:
+        - must be an integer
+        - must be greater than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ method to return current square area of Square """
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """ method to print square """
+        for _ in range(self.__size):
+            if self.__size == 0:
+                print(" ")
+            print("#" * self.__size)
