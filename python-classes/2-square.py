a4fff2b4517634c2dd5bcebad3cc2467e44d6bbd
grahacr <courtneygraham918@gmail.com>
2024-01-15 00:43:54 +0000

    define simple square with private instance attribute size

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module defines a square by private instance attribute size"""
+
+
+class Square:
+    """This class defines a square by private instance attribute size"""
+    def __init__(self, size=0):
+        """This function initializes a simple square if size is digit above 0"""
+        if size.isfloat():
+            self.__size = size
+            elif:
+                raise TypeError("size must be an integer")
+                if size < 0:
+                    raise ValueError("size must be >= 0")
