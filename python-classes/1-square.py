239bd7c7dc56626dae24338397212595317d3a5e
grahacr <courtneygraham918@gmail.com>
2024-01-15 00:35:27 +0000

    pycodestyle

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """ this module defines a square by size """
+
+
 class Square:
     """This class defines a square by private attribute size"""
     def __init__(self, size=0):
         """this function initializes a square by size"""
         self.__size = size
+    
