c6baea9f968ab59cf898faad2b74c080929e6c23
grahacr <courtney.graham@atlasschool.com>
2024-01-15 23:48:07 +0000

    attempt to print area of square with hash signs utilizing position tuple

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """
 """
+
+
 class Square:
     """ Class defines a square """
     def __init__(self, size=0, position=(0, 0)):
         self.__size = size
         self.__position = position
-    
+
     @property
     def size(self):
         """ method to retrieve size of square """
@@ -30,10 +32,23 @@ class Square:
     @position.setter
     def position(self, value):
         """ """
-            if not isinstance(value, tuple) or len(value) != 2:
-                raise TypeError("position must be a tuple of 2 positive integers")
-    
+        if (not isinstance(value, tuple) or
+            len(value) != 2 or
+            not all(isinstance(num, int) for num in value) or
+            not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
     def area(self):
         """ method to return current square area of Square """
         return (self.__size * self.__size)
     
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+            return
+        [print("") for i in range(0, self.__position[1])]
+        for i in range (0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
