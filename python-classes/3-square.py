3f2b79f8f3515e5a5c1e757d37dde9884eb1422c
grahacr <courtneygraham918@gmail.com>
2024-01-15 01:07:34 +0000

    define class square with functions and variables including function to return square area of square if size meets criteria

---
+++ python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module defines a class named Square with variables and functions"""
+
+
+class Square:
+    """This class defines a square with size"""
+    def __init__(self, size=0):
+        """This method initiates a square with integer size above 0"""
+        if size.isfloat():
+            self.__size = size
+            elif size < 0:
+                raise ValueError("size must be >= 0")
+            elif:
+                raise TypeError("size must be an integer")
+    def area(self):
+        """This method returns the square area of size"""
+        if self.size:
+            sqArea = size x size
+            return sqArea
