ff37e817232a671e00de7db14503fb65d5b9c41f
grahacr <courtneygraham918@gmail.com>
2024-01-15 00:34:18 +0000

    new file defines class square and initializes with private att size

---
+++ python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" this module defines a square by size """
+class Square:
+    """This class defines a square by private attribute size"""
+    def __init__(self, size=0):
+        """this function initializes a square by size"""
+        self.__size = size
