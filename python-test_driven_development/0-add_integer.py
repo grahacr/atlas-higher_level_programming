f3ac03bd0595eada35282574c6cc36076948f2bf
grahacr <courtney.graham@atlasschool.com>
2024-01-16 00:48:19 +0000

    initial function file, no test file yet

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" this module defines function that adds two integers """
+def add_integer(a, b=98):
+    """ This function adds two integers and returns result """
+    if type(a) != int or type(a) != float:
+        raise TypeError("a must be an integer")
+    if type(a) != int or type(b) != float:
+        raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        return int(a)
+    if isinstance(b, float):
+        return int(b)
+    return (a + b)
