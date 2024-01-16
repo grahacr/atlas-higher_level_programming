6be788272cbc335f49721b312ba9ee1a65ac6423
grahacr <courtney.graham@atlasschool.com>
2024-01-16 21:08:30 +0000

    minor typo fixes

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ module creates a class named Rectangle """
+
+
 class Rectangle:
     """ class creates rectangle with height and width attributes """
     def __init__(self, width=0, height=0):
@@ -7,14 +9,14 @@ class Rectangle:
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
         elif width < 0:
-            raise ValueError("width must be >=0")
+            raise ValueError("width must be >= 0")
         self.__width = width
         if not isinstance(height, int):
             raise TypeError("height must be an integer")
         elif height < 0:
-            raise ValueError("height must be >=0")
+            raise ValueError("height must be >= 0")
         self.__height = height
-    
+
     @property
     def width(self):
         """ method to retrieve width of rectangle """
