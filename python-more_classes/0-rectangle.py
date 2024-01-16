13c0e0d749dd32e91a2c0b0abf1a0f30b4703722
grahacr <courtney.graham@atlasschool.com>
2024-01-16 20:20:48 +0000

    file to define empty rectangle class

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+""" this module defines a class named rectangle """
+class Rectangle:
+    """ this class defines an empty rectangle """
+    pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+This project is for learning about classes...even more! huzzah!
