c6287a8a549e1e87a6268fe9da7ba5130f7b4a56
grahacr <courtney.graham@atlasschool.com>
2024-01-17 04:33:52 +0000

    function indents and prints text based on special characters

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" """
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    special_chars = ['.', '?', ':']
+    for char in special_chars:
+        parts = text.split(char)
+        for i in range(len(parts)-1):
+            print(parts[i], end="")
+            print(char)
+            print("\n")
