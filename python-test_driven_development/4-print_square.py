cb3ca7f0acfa93e83f9946c1ecc3b13e77218ad8
grahacr <courtney.graham@atlasschool.com>
2024-01-17 03:49:41 +0000

    function and test file for printing a square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" """
+def print_square(size):
+    """ function to print square """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise TypeError("size must be >= 0")
+    elif isinstance(size, float and size < 0):
+        raise TypeError("size must be an integer")
+    for _ in range(size):
+        print("#" * size)
