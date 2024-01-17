abdd8b8fc9198b70f2e51bc851a6b672c3fca250
grahacr <courtney.graham@atlasschool.com>
2024-01-17 03:54:31 +0000

    fix incorrect parentheses and documentation

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" """
+""" module contains function for printing square """
 def print_square(size):
     """ function to print square """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     elif size < 0:
         raise TypeError("size must be >= 0")
-    elif isinstance(size, float and size < 0):
+    elif isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
     for _ in range(size):
         print("#" * size)
