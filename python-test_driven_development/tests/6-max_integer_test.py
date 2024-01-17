b40f4c265209b9099ecadf18c6d725a3748e3303
grahacr <courtney.graham@atlasschool.com>
2024-01-17 20:49:07 +0000

    fix test cases and name of class

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -2,24 +2,32 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-class max_integer_test(unittest.TestCase):
+class TestMaxInteger(unittest.TestCase):
     """ """
     def test_ordered_list(self):
         """ test ordered list of integers """
-        self.assertEqual(max_integer(ordered = [1, 2, 4, 6]), 6)
+        self.assertEqual(max_integer(test_list = [1, 2, 4, 6]), 6)
 
     def test_unordered_list(self):
         """ test unordered list of integers """
-        self.assertEqual(max_integer(unordered = [3, 1, 7, 2]), 7)
-    
+        self.assertEqual(max_integer(test_list = [3, 1, 7, 2]), 7)
+
     def test_empty_list(self):
         """ test empty list """
-        self.assertEqual(max_integer(empty = []), None)
-    
+        self.assertEqual(max_integer(test_list = []), None)
+
     def test_one_element(self):
         """ test list with only one element """
-        self.assertEqual(max_integer(one_element = [3]), 3)
+        self.assertEqual(max_integer(test_list = [3]), 3)
+
+    def test_negative_numbers(self):
+        """ test list with negative numbers """
+        self.assertEqual(max_integer(test_list = [-5, -8, -9, -11]), -5)
+
+    def test_mixed_numbers(self):
+        """ test list of numbers with positive and negative values """
+        self.assertEqual(max_integer(test_list = [8, 4, -2, -4]), 8)
     
-    def test_string(self):
-        """ test string as input """
-        self.assertEqual(max_integer(streeng = ["Hello"]))
+    def test_float_numbers(self):
+        """ test list of floats """
+        self.assertEqual(max_integer(test_list = [4.3, 1.7, 9.8]), 9.8)
