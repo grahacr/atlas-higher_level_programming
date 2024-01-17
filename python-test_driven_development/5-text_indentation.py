b193649869b064ae7bb196c887c7cffe538ee65d
grahacr <courtney.graham@atlasschool.com>
2024-01-17 04:47:19 +0000

    fix output by fixing iteration and printing

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ module contains text_indendation function """
+
+
 def text_indentation(text):
     """ function to separate and print text at special characters """
     if not isinstance(text, str):
@@ -7,7 +9,9 @@ def text_indentation(text):
     special_chars = ['.', '?', ':']
     for char in special_chars:
         parts = text.split(char)
-        for i in range(len(parts)-1):
-            print(parts[i], end="")
-            print(char)
-            print("\n\n")
+        if len(parts) > 1:
+            for i in range(len(parts)-1):
+                print(parts[i], end="")
+                print(char)
+                print("\n\n")
+        print(text)
