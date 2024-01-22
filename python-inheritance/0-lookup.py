0a992f754fbd1ec7106e510cb9425ab889e8b892
grahacr <courtney.graham@atlasschool.com>
2024-01-22 17:08:36 -0600

    documentation

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+""" This module looks at available attributes and methods of object """
 def lookup(obj):
+    """ This function returns the directory of an object """
     return(dir(obj))
