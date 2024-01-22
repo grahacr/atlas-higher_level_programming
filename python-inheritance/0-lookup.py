654fbcbf1d4641ae1d71e943d32b657da1254554
grahacr <courtney.graham@atlasschool.com>
2024-01-22 17:11:10 -0600

    should fix pycodestyle

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -2,6 +2,6 @@
 """ This module looks at available attributes and methods of object """
 
 
-def lookup (obj):
+def lookup(obj):
     """ This function returns the directory of an object """
-    return(dir(obj))
+    return (dir(obj))
