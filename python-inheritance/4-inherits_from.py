cb1d51a6455781f9de39fe3a7b25ae1c0b06d83a
grahacr <courtney.graham@atlasschool.com>
2024-01-22 20:17:59 -0600

    switch to checking for type

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,4 +3,4 @@
 def inherits_from(obj, a_class):
     """ define function that checks if object is instance of class
     - can be either directly or indirectly inherited from specific class """
-    return isinstance (obj, a_class) or issubclass(obj.__class__, a_class)
+    return isinstance (obj, a_class) or issubclass(type(obj), a_class)
