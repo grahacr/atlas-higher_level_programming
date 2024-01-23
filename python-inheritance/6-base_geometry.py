cd9ee36cc3056709139fad92f80c83e2fb6b6ba6
grahacr <courtney.graham@atlasschool.com>
2024-01-22 20:24:33 -0600

    create class with exception function

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" module defines non-empty class """
+class BaseGeometry:
+    """ defines class named BaseGeometry """
+    def area(self):
+        """function to return exception message """
+        raise Exception("area() is not implemented")
