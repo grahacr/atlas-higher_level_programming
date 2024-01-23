956439ce46fa53718bbdcff6eb8276e39dd2dc45
grahacr <courtney.graham@atlasschool.com>
2024-01-22 20:03:28 -0600

    pycodestyle

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ module defines function is_same_class """
+
+
 def is_same_class(obj, a_class):
     """ function checks if object is instance of specified class """
     return obj.__class__ == a_class
