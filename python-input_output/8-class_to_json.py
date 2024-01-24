f8eb532eeb5cc647d671f98c7ff9bd352691e21a
grahacr <courtney.graham@atlasschool.com>
2024-01-23 23:14:48 -0600

    misunderstood question, was much simpler

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -4,6 +4,4 @@
 
 def class_to_json(obj):
     """ function returns dict description using json serialization"""
-    dict_obj = obj.to_dict()
-    json_dict = json.dumps(dict_obj)
-    return json_dict
+    return obj.__dict_
