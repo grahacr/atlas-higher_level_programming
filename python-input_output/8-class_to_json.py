a10f8c15d9edf89b6773ac5708ca9e6b338a1d04
grahacr <courtney.graham@atlasschool.com>
2024-01-23 23:09:10 -0600

    documentingggg

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-""" """
+""" module includes function to convert class object """
 
 
 def class_to_json(obj):
+    """ function returns dict description using json serialization"""
     dict_obj = obj.to_dict()
     json_dict = json.dumps(dict_obj)
     return json_dict
