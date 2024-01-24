2d3558c72c077f711e0cd297e6134701336fc30b
grahacr <courtney.graham@atlasschool.com>
2024-01-23 22:17:11 -0600

    documentation and typo

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python
-""" """
+#!/usr/bin/python3
+""" module contains function for converting input to json """
 import json
 
 
 def to_json_string(my_obj):
+    """function takes input object - string - and converts to json """
     json_vers = json.dumps(my_obj)
     return json_vers
