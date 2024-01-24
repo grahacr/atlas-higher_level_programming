0589c1d6b3ef66b1b0aeff1a35a7551b0331519c
grahacr <courtney.graham@atlasschool.com>
2024-01-23 20:19:24 -0600

    same as last module but utilize str function

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" module creates class Rectangle from inherited BaseGeometry class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Class Rectangle subclass of BaseGeometry """
+    def __init__(self, width, height):
+        """ initialize instance of Rectangle """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+    
+    def area(self):
+        """ utilize area method of parent class """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """print string representation of Rectangle object"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
