887b8cc88cbc8e41c071b789a2c0e23fffadd56d
grahacr <courtney.graham@atlasschool.com>
2024-01-23 22:05:48 -0600

    write to file and return number of characters written

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""module defines read file function"""
+
+
+def write_file(filename="", text=""):
+    """ function to write to file and return number of characters"""
+    with open(filename, mode='w', encoding='utf-8') as a_file:
+        a_file(write(text))
+        num_chars = len(text)
+        return num_chars
