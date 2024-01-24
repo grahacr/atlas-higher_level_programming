038430be6776b2f8bb2dd6a4f1c3940b63f24012
grahacr <courtney.graham@atlasschool.com>
2024-01-23 23:21:02 -0600

    documentation

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-""" """
+""" module contains class Student """
+
+
 class Student(self):
-    """ class student initializes student objects """
+    """ class Student initializes student objects """
     def __init__(self, first_name, last_name, age):
         self.firstname = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """ function to return dictionary of Student """
         return self.__dict__
