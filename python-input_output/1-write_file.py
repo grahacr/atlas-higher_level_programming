44c32097df18db408e9b5dfa9f89adb1426aefa2
grahacr <courtney.graham@atlasschool.com>
2024-01-23 22:07:24 -0600

    unnecessary parenthesis

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,6 +6,6 @@
 def write_file(filename="", text=""):
     """ function to write to file and return number of characters"""
     with open(filename, mode='w', encoding='utf-8') as a_file:
-        a_file(write(text))
+        a_file.write(text)
         num_chars = len(text)
         return num_chars
