518a88903a501d5c444a10a536013e8ada1b6efd
grahacr <courtney.graham@atlasschool.com>
2024-01-23 22:40:36 -0600

    function loads object from json file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -4,7 +4,7 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    """ function writes object to text file using json representation"""
+    """ function writes object to text file using json"""
     json_vers = json.dumps(my_obj)
     with open(filename, mode='w', encoding='utf-8') as a_file:
         a_file.write(json_vers)
---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" module defines function to load object from json file"""
+import json
+
+
+def load_from_json_file(filename):
+    """ this function creates python object from json file"""
+    with open(filename, mode='r', encoding='utf-8') as a_file:
+        return json.loads(a_file)
