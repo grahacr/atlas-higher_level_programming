d83264cf3a87462176ffd085452b291de7e67bfd
grahacr <courtney.graham@atlasschool.com>
2024-01-26 20:14:33 +0000

    finish creating base class and id logic

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -6,6 +6,9 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """ initialize instance of Base class with id """
         if id is not None:
             self.id = id
-        else += __nb_objects
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
