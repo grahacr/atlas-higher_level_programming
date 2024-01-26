e1dc61e7c807d0a0c89e66bd1d2c214320ff7509
grahacr <courtney.graham@atlasschool.com>
2024-01-26 20:35:54 +0000

    Rectangle class with private instance attributes and getters and setters

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" Module for subclass Rectangle"""
+class Rectangle(Base):
+    """ create Rectangle class inherited from Base class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ initialize rectangle class with private attributes """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
