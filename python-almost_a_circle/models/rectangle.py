d67df45e1e9331e6a46f50009ba28c452e92b16b
grahacr <courtney.graham@atlasschool.com>
2024-01-28 18:48:46 +0000

    fix formatting for desired output

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -78,4 +78,4 @@ class Rectangle(Base):
         print(display_str)
 
     def __str__(self):
-        return (f"[Rectangle]{self.id} {self.x}/{self.y} - {self.width}/{self.height}")
+        return (f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}")
