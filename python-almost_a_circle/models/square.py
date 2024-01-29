99c2b591d1f44661b856020cec4357d67af4167c
grahacr <courtney.graham@atlasschool.com>
2024-01-29 11:38:39 -0600

    add square update public method

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -26,3 +26,23 @@ class Square(Rectangle):
             raise ValueError("width must be > 0")
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.size = args[1]
+        if len(args) >= 3:
+            self.x = args[2]
+        if len(args) >= 4:
+            self.y = args[3]
+        for key, value in kwargs.item():
+            if key == 'id':
+                self.id = value
+            elif key == 'size':
+                self.width = value
+                self.height = value
+            elif key == 'x':
+                self.x = value
+            elif key == 'y':
+                self.y = value
