8924c881f1a133968a62c1a98d4a1eade7b31a24
grahacr <courtney.graham@atlasschool.com>
2024-01-29 09:53:39 -0600

    whitespace

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -77,7 +77,7 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.x, self.y, self.width, self.height)
 
-    def update(self,*args):
+    def update(self, *args):
         """ update attributes based on command line args """
         if len(args) >= 1:
             self.id = args[0]
