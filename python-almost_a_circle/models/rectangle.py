d484bcfffafc1241543c9c078049603f397cf59e
grahacr <courtney.graham@atlasschool.com>
2024-01-29 09:52:43 -0600

    arg typo

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -80,12 +80,12 @@ class Rectangle(Base):
     def update(self,*args):
         """ update attributes based on command line args """
         if len(args) >= 1:
-            self.id = arg[0]
+            self.id = args[0]
         if len(args) >= 2:
-            self.width = arg[1]
+            self.width = args[1]
         if len(args) >= 3:
-            self.height = arg[2]
+            self.height = args[2]
         if len(args) >= 4:
-            self.x = arg[3]
+            self.x = args[3]
         if len(args) >= 5:
-            self.y = arg[4]
+            self.y = args[4]
