d22e27595c3233f5ed2295e93160e3d14f60f4b3
grahacr <courtney.graham@atlasschool.com>
2024-01-29 11:21:32 -0600

    getter and setter for square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -12,4 +12,17 @@ class Square(Rectangle):
     def __str__(self):
         """ replace string conductor to print attributes of square """
         return "[Square] ({}) {}\{} - {}".format(
-            self.id, self.x, self.y, self.height)
+            self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        return self.width
+    
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
