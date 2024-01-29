0bd42510adaa08421a1e1d24524ec8e4efbcd491
grahacr <courtney.graham@atlasschool.com>
2024-01-29 10:12:29 -0600

    class Square inheriting attributes from Rectangle

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" module for subclass Square via Rectangle"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ create Square class inherited from Rectangle class """
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ replace string conductor to print attributes of square """
+        return "[Square] ({}) {}\{} - {}".format(
+            self.id, self.x, self.y, self.size)
