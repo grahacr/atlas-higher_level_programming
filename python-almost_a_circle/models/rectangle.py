b08f1a5983852bdf00a8d65abce7313a21a198f4
grahacr <courtney.graham@atlasschool.com>
2024-01-29 09:59:35 -0600

    modify update method to account for kwargs

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -77,15 +77,26 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.x, self.y, self.width, self.height)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """ update attributes based on command line args """
         if len(args) >= 1:
             self.id = args[0]
-        if len(args) >= 2:
+            elif if len(args) >= 2:
             self.width = args[1]
-        if len(args) >= 3:
+            elif len(args) >= 3:
             self.height = args[2]
-        if len(args) >= 4:
+            elif len(args) >= 4:
             self.x = args[3]
-        if len(args) >= 5:
+            elif len(args) >= 5:
             self.y = args[4]
+        for key, value in kwargs.items():
+            if key == 'id':
+                self.id = value
+            elif key == "width":
+                self.width = value
+            elif key == "height":
+                self.height = value
+            elif key == "x":
+                self.x = value
+            elif key == "y":
+                self.y =value
