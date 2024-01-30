508ee75e43a577521b1264820c9c89f055ee0da4
grahacr <courtney.graham@atlasschool.com>
2024-01-29 20:28:07 -0600

    add class method save to file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -17,6 +17,17 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """ method for converting to json string"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return ('[]')
         return json.dumps(list_dictionaries)
+
+    def save_to_file(cls, list_objs):
+        """ class method to save json strings to file"""
+        if list_objs is None:
+            list_objs = []
+        filename = "{}.json".format(cls.__name__)
+        with open(filename, 'w') as file:
+            dict_json = [obj.to_dictionary() for obj in list_objs]
+            string_json = cls.to_json_string(dict_json)
+            file.write(string_json)
