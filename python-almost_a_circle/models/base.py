5a44c51b3f6d3ce49347afbfd7ffe30d7d09354c
grahacr <courtney.graham@atlasschool.com>
2024-01-29 19:51:49 -0600

    list to json string static method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ this module includes class Base"""
+import json
 
 
 class Base:
@@ -13,3 +14,7 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        return json.dumps(list_dictionaries)
