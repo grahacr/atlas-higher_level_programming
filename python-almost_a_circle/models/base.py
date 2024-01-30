8c0a656c88db84c7c22a29eaa0e3e56f35212787
grahacr <courtney.graham@atlasschool.com>
2024-01-29 19:56:33 -0600

    minor fixes

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -18,6 +18,5 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            return('[]')
+            return ('[]')
         return json.dumps(list_dictionaries)
-
