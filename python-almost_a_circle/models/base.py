e75a5d9104f76a89dbaa45a73880add446a1a8dd
grahacr <courtney.graham@atlasschool.com>
2024-01-29 19:55:46 -0600

    check for edge case

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -17,4 +17,7 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return('[]')
         return json.dumps(list_dictionaries)
+
