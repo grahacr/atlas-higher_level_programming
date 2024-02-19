ded508ad3666a007928e2914d0e58448357d8231
grahacr <courtney.graham@atlasschool.com>
2024-02-19 23:40:37 +0000

    create database and second user with select privileges

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- create database and user with certain permissions
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2 IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT PRIVILEGES ON hbtn_0d_2 TO user_0d_2;
