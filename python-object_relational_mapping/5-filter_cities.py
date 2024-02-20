77bf54d41364f73446ff96d549cf5ee354a8117d
grahacr <courtney.graham@atlasschool.com>
2024-02-20 22:19:44 +0000

    print cities associated with given state passed as parameter

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+list cities associated with given state name
+"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+    )
+    cursor = connection.cursor()
+    state = sys.argv[4]
+    cursor.execute(
+        """SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id
+        WHERE LOWER(states.name) LIKE LOWER(%s)
+        ORDER BY cities.id ASC;""",
+        (state + '%',)
+    )
+    cities = cursor.fetchall()
+    for city in cities:
+        print(city)
+    cursor.close()
+    connection.close()
