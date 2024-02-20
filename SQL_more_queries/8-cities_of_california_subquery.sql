0ffa908539ed7fa59859fabbf329a2fdc126cc57
grahacr <courtney.graham@atlasschool.com>
2024-02-20 02:18:57 +0000

    sorting by wrong table

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- cities of california in database hbtn_0d_usa
 
 SELECT 
-(SELECT name FROM cities WHERE state_id = id) AS city
+    (SELECT name FROM cities WHERE state_id = id) AS city
 FROM states
-ORDER BY cities.id ASC;
+ORDER BY states.id ASC;
