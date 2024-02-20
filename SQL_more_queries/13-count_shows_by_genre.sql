5a52bcf9be94f883a3d9113ae37f4aef53a2361e
grahacr <courtney.graham@atlasschool.com>
2024-02-20 05:15:04 +0000

    small errors

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- list all genres and count of linked shows
 
 SELECT tv_genres.name AS genre,
-COUNT (tv_show_genres.genre_id) as number_of_shows
+COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_show_genres INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
