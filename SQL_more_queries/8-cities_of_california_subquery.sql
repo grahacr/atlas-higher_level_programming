de25a042c956d9ff9093c9e582b95456e1d7d425
grahacr <courtney.graham@atlasschool.com>
2024-02-20 02:22:14 +0000

    fix subquery syntax

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,5 @@
 -- cities of california in database hbtn_0d_usa
 
-SELECT 
-    (SELECT name FROM cities WHERE state_id = id) AS city
-FROM states
-ORDER BY states.id ASC;
+SELECT id, name FROM cities WHERE state_id = 
+    (SELECT id from states where name = 'California')
+ORDER BY cities.id ASC;
