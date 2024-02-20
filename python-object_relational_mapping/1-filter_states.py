328a22bc052dc7a353bf5b45af2ff72db0991ac8
grahacr <courtney.graham@atlasschool.com>
2024-02-20 19:17:49 +0000

    save before losing - not done yet

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" script to list all states
+specifically with name starting with n """
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(
+        host="localhost".
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+    )
+    cursor = connection.cursor()
+    query = SELECT id, name FROM states WHERE name = ""
