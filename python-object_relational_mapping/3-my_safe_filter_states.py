25a6c0ddc8ed25b872a716ed7a0019f4c411197f
grahacr <courtney.graham@atlasschool.com>
2024-02-20 20:53:54 +0000

    same as task 2 but protect against sql injection. stop using format

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+display values of states table associated with argument
+and protect against sql injection
+"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+    )
+    cursor = connection.cursor()
+    state = sys.argv[4]
+    cursor.execute(
+        """SELECT id, name FROM states
+        WHERE LOWER(name) LIKE LOWER(%s)
+        ORDER BY states.id ASC;""",
+        (state + %,)
+    )
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    connection.close()
