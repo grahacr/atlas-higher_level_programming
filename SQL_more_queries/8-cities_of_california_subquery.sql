ad9f39d7228f9433166606f187ddb7a0086c84ed
grahacr <courtney.graham@atlasschool.com>
2024-02-20 02:23:16 +0000

    capitalize sql keywords

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- cities of california in database hbtn_0d_usa
 
 SELECT id, name FROM cities WHERE state_id = 
-    (SELECT id from states where name = 'California')
+    (SELECT id FROM states WHERE name = 'California')
 ORDER BY cities.id ASC;
