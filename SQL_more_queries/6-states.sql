ba474b3cfb463c9952f5f0c6197e97f5b67d7a5b
grahacr <courtney.graham@atlasschool.com>
2024-02-20 01:58:51 +0000

    try without use statement

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,6 @@
 -- create database and table with unique auto generated non-null primary key
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
 CREATE table IF NOT EXISTS states (
     id INT NOT NULL PRIMARY KEY,
     name VARCHAR(256)
