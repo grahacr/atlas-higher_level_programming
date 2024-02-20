9f3db3c1e38e131d50ef15ccf46d6d5cbb290419
grahacr <courtney.graham@atlasschool.com>
2024-02-20 02:00:28 +0000

    link database another way

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- create database and table with unique auto generated non-null primary key
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE table IF NOT EXISTS states (
-    id INT NOT NULL PRIMARY KEY,
+CREATE table IF NOT EXISTS hbtn_0d_usa.states (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256)
 );
