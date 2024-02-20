865caa78af7e462cbd045968dca20dfccfb44339
grahacr <courtney.graham@atlasschool.com>
2024-02-20 01:58:25 +0000

    redundancy

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -3,6 +3,6 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE table IF NOT EXISTS states (
-    id INT NOT NULL UNIQUE PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY,
     name VARCHAR(256)
 );
