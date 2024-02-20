f665e42a8444162d81a6531dbfc15d91140691cd
grahacr <courtney.graham@atlasschool.com>
2024-02-20 18:44:52 +0000

    typos and fix module doc

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+""" module to utilize python ORM to run simple sql query """
+
+
 import MySQLdb
-""" utilize python ORM to run simple sql query """
+import sys
 
 if __name__ == "__main__":
     connection = MySQLdb.connect(host="localhost", user="root", password="root", database="hbtn_0e_0_usa")
     cursor = connection.cursor()
     query = "SELECT id, name FROM states ORDER BY states.id ASC"
     cursor.execute(query)
-    rows - cursor.fetchall()
+    rows = cursor.fetchall()
     for row in rows:
         print(row)
     cursor.close()
