b839f89220ec6efbcd3320475ae4c5a516828f0c
grahacr <courtney.graham@atlasschool.com>
2024-02-20 20:34:18 +0000

    indentation again

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
     cursor = connection.cursor()
     state = sys.argv[4]
     query = ("SELECT id, name "
-    "FROM states WHERE name LIKE '{}' "
-    "ORDER BY states.id ASC".format(state + "%"))
+            "FROM states WHERE name LIKE '{}' "
+            "ORDER BY states.id ASC".format(state + "%"))
     cursor.execute(query)
     rows = cursor.fetchall()
     for row in rows:
