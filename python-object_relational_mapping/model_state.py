2c05ac95cee6d89cc0c60ad0dec68df6774d2329
grahacr <courtney.graham@atlasschool.com>
2024-02-21 02:13:51 +0000

    oops tablename must be used

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -6,13 +6,12 @@ module defines base model connected to mySQL table
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
 
 Base = declarative_base()
 
 class State(Base):
     """subclass inherited from Base
     connects to SQL table called states"""
-    __table__ = 'states'
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
