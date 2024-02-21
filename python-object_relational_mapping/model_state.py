65700ee8a7306f84f697dfca272214a8aba447a4
grahacr <courtney.graham@atlasschool.com>
2024-02-21 02:10:19 +0000

    use sqlalchemy to create model class

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+module defines base model connected to mySQL table
+"""
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+import Base
+
+Base = declarative_base()
+
+class State(Base):
+    """subclass inherited from Base
+    connects to SQL table called states"""
+    __table__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
