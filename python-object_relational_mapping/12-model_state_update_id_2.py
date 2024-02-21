65a336f17ad6b57cdac70376fb7e36bd107272be
grahacr <courtney.graham@atlasschool.com>
2024-02-21 06:37:58 +0000

    documentation

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """
+module to update name of state object in database
 """
 
 
@@ -15,17 +16,22 @@ if __name__ == "__main__":
     password = sys.argv[2]
     db = sys.argv[3]
 
-    """ """
+    """create engine to connect to database"""
     engine = create_engine(
         f'mysql://{username}:{password}@localhost/{db}'
     )
-    """ """
+    """collect data from engine"""
     Base.metadata.create_all(engine)
-    """ """
+    """start session using bind"""
     Session = sessionmaker(bind=engine)
+    """create new session instance"""
     session = Session()
+    """query for state matching certain id"""
     state_update = session.query(State).filter(State.id == '2').first()
     if state_update:
+        """update name of State"""
         state_update.name = 'New Mexico'
+        """commit session to database"""
         session.commit()
+    """close session"""
     session.close()
