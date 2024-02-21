b47df34110f5e9ae8406e3120ff75ea0802205ae
grahacr <courtney.graham@atlasschool.com>
2024-02-21 02:16:35 +0000

    pycodestyle

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -12,6 +12,7 @@ Base = declarative_base()
 class State(Base):
     """subclass inherited from Base
     connects to SQL table called states"""
+
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
