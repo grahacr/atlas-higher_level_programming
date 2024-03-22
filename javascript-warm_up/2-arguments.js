ce962358159605d2aa67c90a8e9339b4ed4c362d
grahacr <courtney.graham@atlasschool.com>
2024-03-22 18:19:49 -0500

    fix numbers for argv length

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@
 function varArgs() {
     if (process.argv.length === 2) {
         console.log("No argument");
-    } else if (process.argv.length === 1) {
+    } else if (process.argv.length === 3) {
         console.log("Argument found");
     } else {
         console.log("Arguments found");
