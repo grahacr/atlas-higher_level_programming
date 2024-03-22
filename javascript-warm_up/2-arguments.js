5939dd852385d7ccec31aa381f2004ab079f2785
grahacr <courtney.graham@atlasschool.com>
2024-03-22 18:21:46 -0500

    semistandard fixes

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-function varArgs() {
-    if (process.argv.length === 2) {
-        console.log("No argument");
-    } else if (process.argv.length === 3) {
-        console.log("Argument found");
-    } else {
-        console.log("Arguments found");
-    }
+function varArgs () {
+  if (process.argv.length === 2) {
+    console.log('No argument');
+  } else if (process.argv.length === 3) {
+    console.log('Argument found');
+  } else {
+    console.log('Arguments found');
+  }
 }
 varArgs();
