20128b08298d1c88bdf1ff54e6af200ae0cca5ac
grahacr <courtney.graham@atlasschool.com>
2024-03-22 18:35:04 -0500

    semistandard

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 function concatty () {
-    const arg1 = process.argv[2];
-    const arg2 = process.argv[3];
-    console.log(arg1 + ' is ' + arg2);
+  const arg1 = process.argv[2];
+  const arg2 = process.argv[3];
+  console.log(arg1 + ' is ' + arg2);
 }
 concatty();
