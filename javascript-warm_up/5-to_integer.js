0c90ede7341d7db97c57998ecfad5fc007f8f39a
grahacr <courtney.graham@atlasschool.com>
2024-03-22 19:25:28 -0500

    semistandard fix

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-function intCheck() {
-    const argument = parseInt(process.argv[2]);
-    if (!isNaN(argument)) {
-        console.log("My number: " + argument);
-    } else {
-        console.log("Not a number");
-    }
+function intCheck () {
+  const argument = parseInt(process.argv[2]);
+  if (!isNaN(argument)) {
+    console.log('My number: ' + argument);
+  } else {
+    console.log('Not a number');
+  }
 }
-intCheck()
+intCheck();
