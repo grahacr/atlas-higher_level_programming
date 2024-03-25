b6e654a24014ae96ecefd896a077166730a9e60a
grahacr <courtney.graham@atlasschool.com>
2024-03-25 13:08:59 -0500

    less than equal to?

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ function printC(x) {
   if (isNaN(parseInt(x, 10))) {
     console.log('Missing number of occurences');
   } else {
-    for (let i = 0; i < x; i++) {
+    for (let i = 0; i <= x; i++) {
       console.log('C is fun');
     }
   }
