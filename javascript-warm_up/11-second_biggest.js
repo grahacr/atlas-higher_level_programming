cef4c1f343ef48d67c98dc82a9c136a6078648df
grahacr <courtney.graham@atlasschool.com>
2024-03-25 18:13:57 -0500

    fix integers.length for output

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@ const integers = process.argv.slice(2).map(arg => parseInt(arg));
 integers.sort((a, b) => b - a);
 
 let secondBiggest;
-if (integers.length === 2 || integers.length === 3) {
+if (integers.length === 0 || integers.length === 1) {
   console.log(0);
 } else {
   secondBiggest = integers[1];
