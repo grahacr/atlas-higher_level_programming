ec2855d13afaf64395a67eca3ee6f2b2eec4de51
grahacr <courtney.graham@atlasschool.com>
2024-03-25 13:22:00 -0500

    semistandard

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -8,7 +8,7 @@ if (isNaN(size)) {
   for (let i = 0; i < size; i++) {
     let row = '';
     for (let j = 0; j < size; j++) {
-        row += 'X';
+      row += 'X';
     }
     console.log(row);
   }
