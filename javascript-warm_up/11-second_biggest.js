0ab7016b535efe97d564bee7719084b21efc1577
grahacr <courtney.graham@atlasschool.com>
2024-03-25 18:12:15 -0500

    semistandard fix

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
-const integers = process.argv.slice(2).map(arg => parseInt(arg))
+const integers = process.argv.slice(2).map(arg => parseInt(arg));
 
 integers.sort((a, b) => b - a);
 
 let secondBiggest;
 if (integers.length === 2 || integers.length === 3) {
-    return 0
+  console.log(0);
 } else {
-    secondBiggest = integers[1];
+  secondBiggest = integers[1];
 }
-console.log(secondBiggest)
+console.log(secondBiggest);
