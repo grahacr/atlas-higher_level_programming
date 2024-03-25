86b3bea35ad5c4acb12707ae748f78f539ef1771
grahacr <courtney.graham@atlasschool.com>
2024-03-25 13:14:14 -0500

    not necessary to encapsulate in function, create script only and process argv

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-function printC(x) {
-  if (isNaN(parseInt(x, 10))) {
-    console.log('Missing number of occurences');
-  } else {
-    for (let i = 0; i <= x; i++) {
-      console.log('C is fun');
-    }
+const x = process.argv[2];
+
+if (isNaN(x)) {
+  console.log('Missing number of occurences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
   }
 }
