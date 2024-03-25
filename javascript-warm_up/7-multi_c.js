bdba8509c8ac23d65c99831fc136ef30c02a47b8
grahacr <courtney.graham@atlasschool.com>
2024-03-25 13:06:53 -0500

    print c is cool a certain number of times.

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+function printC(x) {
+  if (isNaN(parseInt(x, 10))) {
+    console.log('Missing number of occurences');
+  } else {
+    for (let i = 0; i < x; i++) {
+      console.log('C is fun');
+    }
+  }
+}
