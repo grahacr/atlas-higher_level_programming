d7a6ee985cee3362b3693363a19e5a0f629e5071
grahacr <courtney.graham@atlasschool.com>
2024-03-25 17:04:52 -0500

    fix base case

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -3,7 +3,7 @@
 const number = parseInt(process.argv[2]);
 
 function factorialRecursive (number) {
-  if (number === 0 || number === 1) {
+  if (isNaN(number) || number < 0 || number === 0) {
     return 1;
   }
   return number * factorialRecursive(number - 1);
