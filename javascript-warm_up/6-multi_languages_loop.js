a774699c3008bb937588aa0d004bf7987582007b
grahacr <courtney.graham@atlasschool.com>
2024-03-25 12:57:37 -0500

    semistandard fixes

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 function languagePrint () {
-    const strings = ["C is fun", "Python is cool", "JavaScript is amazing"];
-    for (let i = 0; i < strings.length; i++) {
-        console.log(strings[i]);
-    }
+  const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+  for (let i = 0; i < strings.length; i++) {
+    console.log(strings[i]);
+  }
 }
 
-languagePrint()
+languagePrint();
