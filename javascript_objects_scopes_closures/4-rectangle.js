34842f8cd650052e2d758c806f7b1934aa3a9c2c
grahacr <courtney.graham@atlasschool.com>
2024-03-26 16:24:23 -0500

    add rotate function to rectangle class, not done with task tho

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w >= 1 && h >= 1) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+  rotate() {
+    const temp = this.width
+    this.width = this.height;
+    this.height = temp;
+  }
+}
+
+module.exports = Rectangle;
