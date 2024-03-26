0101d7ca701c33074b1cc1f558004709a5e3a1e1
grahacr <courtney.graham@atlasschool.com>
2024-03-26 15:51:30 -0500

    fix values to be undefined aka empty?

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,6 +3,8 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0) {
+      this.width = undefined;
+      this.height = undefined;
     } else {
       this.width = w;
       this.height = h;
