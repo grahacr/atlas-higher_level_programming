320449f9e83caad1332a2c35b0ed67ba7ef63991
grahacr <courtney.graham@atlasschool.com>
2024-03-26 15:53:53 -0500

    ugh idk, fix the empty class i hope

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,8 +3,7 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0) {
-      this.width = undefined;
-      this.height = undefined;
+
     } else {
       this.width = w;
       this.height = h;
