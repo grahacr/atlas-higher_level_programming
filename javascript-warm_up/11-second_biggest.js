fc72a5c2865829ab08fc593ac7a2d837d8b10d4f
grahacr <courtney.graham@atlasschool.com>
2024-03-26 12:31:26 -0500

    fix return so it says 0 without "undefined" as well

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -5,8 +5,10 @@ const integers = process.argv.slice(2).map(arg => parseInt(arg));
 integers.sort((a, b) => b - a);
 
 let secondBiggest;
-if (integers.length === 0 || integers.length === 1) {
-  console.log(0);
+if (integers.length === 0) {
+  secondBiggest = 0;
+} else if (integers.length === 1) {
+  secondBiggest = integers[0];
 } else {
   secondBiggest = integers[1];
 }
