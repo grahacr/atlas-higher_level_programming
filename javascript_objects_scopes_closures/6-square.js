52f14ca86f95192057a02ba4ab0d63afef2e621b
grahacr <courtney.graham@atlasschool.com>
2024-03-26 20:49:20 -0500

    fix identical square names for output

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const Square = require('./5-square');
+const square = require('./5-square');
 
-class Square extends Square {
+class Square extends square {
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
