149d2deff5a5bbd8f3397720a060a9ccd4c9a59b
grahacr <courtney.graham@atlasschool.com>
2024-03-26 20:47:18 -0500

    create another square class that prints the rectangle

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const Square = require('./5-square');
+
+class Square extends Square {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += c;
+      }
+      console.log(row);
+    }
+  }
+}
+module.exports = Square;
