522d88764511d236f9caaa50a357d754f4d10a98
grahacr <courtney.graham@atlasschool.com>
2024-04-01 18:13:03 -0500

    fix how i looked for character id and broke down lists, also used character url directly instead of split

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,23 +2,20 @@
 
 const request = require('request');
 const url = process.argv[2];
-const findId = '18';
+const findId = '18/';
+const charUrl = 'https://swapi-api.hbtn.io/api/people/';
 
 request.get(url, (error, response, body) => {
   if (error) {
     console.error(error);
     return;
   }
-  const data = JSON.parse(body);
+  const data = JSON.parse(body).results;
   let count = 0;
-  data.results.forEach(film => {
-    if (film.characters) {
-      film.characters.forEach(character => {
-        const characterId = character.split('/').pop();
-        if (characterId === findId) {
-          count++;
-        }
-      })
+  data.forEach(film => {
+    const characters = film.characters;
+    if (characters.include(charUrl + findId)) {
+      count++;
     }
   });
   console.log(count);
