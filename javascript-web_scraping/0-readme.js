adc5c1822e78586c5cad44fbeaa459d11f8054ec
grahacr <courtney.graham@atlasschool.com>
2024-04-01 12:47:59 -0500

    semistandard

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -5,9 +5,9 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
