5ecce022d49374f02307fc2c9e6d29411ca06ad3
grahacr <courtney.graham@atlasschool.com>
2024-04-01 13:05:14 -0500

    use request module to display status code of get request

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request.get(url, (response, body) => {
+  if (response) {
+    console.log('code:', response.statusCode);
+  }
+});
