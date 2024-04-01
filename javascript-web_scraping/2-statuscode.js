5457649e3ccf6923d8509e2e678117afc17e52d5
grahacr <courtney.graham@atlasschool.com>
2024-04-01 13:10:31 -0500

    semistandard

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -5,11 +5,11 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, (error, response, body) => {
-    if (error) {
-        console.error(error);
-        return;
-    }
-    if (response) {
-        console.log('code:', response.statusCode);
+  if (error) {
+    console.error(error);
+    return;
+  }
+  if (response) {
+    console.log('code:', response.statusCode);
   }
 });
