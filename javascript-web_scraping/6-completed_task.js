a89d201f1381073c745aa3eafd247d9bcbb46aae
grahacr <courtney.graham@atlasschool.com>
2024-04-01 22:33:37 -0500

    print number of completed tasks for each user id

---
+++ javascript-web_scraping/6-completed_task.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+const apiTask = process.argv[2];
+
+request.get(apiTask, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const tasks = JSON.parse(body);
+  const userCompleted = new Map();
+  tasks.forEach(task => {
+    const userId = task.userId;
+    const completed = task.completed;
+    if (completed) {
+      if (userCompleted.has(userId)) {
+        userCompleted.set(userId, userCompleted.get(userId) + 1);
+      } else {
+        userCompleted.set(userId, 1);
+      }
+    }
+  });
+  userCompleted.forEach((count, userId) => {
+    console.log(`'${userId}: ${count}`);
+  });
+});
